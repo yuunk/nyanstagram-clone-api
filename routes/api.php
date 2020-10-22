c5fa511ece9cd5a1096970787fdf1b6ed9c701f9
yuunk <dchipself0@gmail.com>
2020-10-22 20:56:51 +0900

    update: laravel re

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::group(['middleware' => ['api']], function () {
-    Route::get('/hoge', 'Api\hogeController@index');
-    Route::get('/tweet', 'Api\TweetController@index');
-});
